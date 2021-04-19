998b9a68128b57c989f36f9faa4f3b4c8f75525c
Jakob Persons <jakobep24@gmail.com>
2021-04-19 16:24:30 -0400

    databse created

---
+++ db/migrate/20210419194149_create_users.rb
@@ -0,0 +1,23 @@
+class CreateUsers < ActiveRecord::Migration[6.1]
+  def change
+    create_table :users do |t|
+      t.string :username
+      t.string :password_digest
+      t.string :gender
+      t.integer :age
+      t.string :sexual_orientation
+      t.string :race
+      t.string :city
+      t.string :political_party
+      t.string :religion
+      t.boolean :pro_choice
+      t.string :fav_sport
+      t.string :fav_cuisine
+      t.string :fav_book_genre
+      t.string :fav_city
+      t.string :fav_movie_genre
+
+      t.timestamps
+    end
+  end
+end
