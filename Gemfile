7570374579db2db867e8407454508021ae18a355
Jakob Persons <jakobep24@gmail.com>
2021-04-20 08:49:29 -0400

    tried starting chat feature

--- Gemfile
+++ Gemfile
@@ -22,6 +22,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+# stream chat gem
+gem 'stream-chat-ruby', '~> 2.6'
+
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,13 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
-require 'stream-chat'
+# require 'stream-chat'
+
+# puts 'creating user'
+# jake = User.create(username: "jpersons", password: "123", gender: "male", age: 28)
 
 # instantiate stream client using API key and secret
-client = StreamChat::Client.new(api_key='9tbsyvz84awf', api_key='xvzzd4ws4tmkfexkg832mq56944vqdhndakarrmkvynvwscagmw97humdymtt2v8')
-client.create_token('jake')
+# client = StreamChat::Client.new(api_key='9tbsyvz84awf', api_secret='xvzzd4ws4tmkfexkg832mq56944vqdhndakarrmkvynvwscagmw97humdymtt2v8')
+# client.create_token(jake)
 
 puts 'success!!'
