661b07802f4ad46b62a530aac7c69f481d5bd8ca
Ori Markowitz <ori.markowitz@gmail.com>
2021-04-23 14:51:59 -0400

    moved algo to backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,100 @@ class UsersController < ApplicationController
 
       user.update(user_params)
       render json: { status: true, user: user, token: chat_token(user.username) }
-    end 
+    end
+    
+    def match
+      user = User.find(params[:id])
+      found_user = nil
+      if user.previous_matches.length > 0
+        found_user = User.find_by(username: user.previous_matches[0])
+        render json: { status: true, user: found_user, token: chat_token(found_user.username) }
+        return
+      end
+      filtered_users = User.all.filter do |user_option|
+        user_option.is_chatting === false
+      end
+      new_connections = filtered_users.filter do |user_option|
+        !user.previous_matches.include?(user_option.username)
+      end
+
+      collected_array = []
+      final_differences = []
+      final_interests = []
+
+      new_connections.each do |user_option|
+        differences_array = []
+        interests_array = []
+
+        belief_counter = 0
+        interest_counter = 0
+
+        if user_option.gender != user.gender
+          belief_counter += 1
+          differences_array.push("gender")
+        end
+
+        if user_option.sexual_orientation != user.sexual_orientation
+          belief_counter += 1
+          differences_array.push("sexual orientation")
+        end
+
+        if user_option.race != user.race
+          belief_counter += 1
+          differences_array.push("race")
+        end
+
+        if user_option.religion != user.religion
+          belief_counter += 1
+          differences_array.push("religion")
+        end
+
+        if user_option.pro_choice != user.pro_choice
+          belief_counter += 1
+          differences_array.push("abortion")
+        end
+
+        if user_option.political_party != user.political_party
+          belief_counter += 1
+          differences_array.push("politics")
+        end
+
+        if user_option.fav_sport == user.fav_sport
+          interest_counter += 1
+          interests_array.push("favorite sport")
+        end
+
+        if user_option.fav_cuisine == user.fav_cuisine
+          interest_counter += 1
+          interests_array.push("favorite cuisine")
+        end
+
+        if user_option.fav_book_genre == user.fav_book_genre
+          interest_counter += 1
+          interests_array.push("favorite book genre")
+        end
+
+        if user_option.fav_city == user.fav_city
+          interest_counter += 1
+          interests_array.push("favorite city")
+        end
+
+        if user_option.fav_movie_genre == user.fav_movie_genre
+          interest_counter += 1
+          interests_array.push("favorite movie genre")
+        end
+
+        if belief_counter >= 3 && interest_counter >= 3
+          collected_array.push(user_option)
+          final_differences.push(differences_array)
+          final_interests.push(interests_array)
+        end
+      end
+      user.update(differences: final_differences[0], interests: final_interests[0])
+      found_user = User.find_by(username: collected_array[0].username)
+      found_user.update(differences: final_differences[0], interests: final_interests[0])
+      render json: { status: true, user: found_user, token: chat_token(found_user.username) }
+    end
   
     private
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, only: [:index, :create, :update]
   
   # /login to login controller
   post "/login", to: "users#login"
+  get "/users/:id", to: "users#match"
 end
